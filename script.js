209abd9471cf20bfd74b330fd79eec52f35c5580
error-mesage-404 <wapex999@gmail.com>
2025-11-20 17:37:42 +0100

    .

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 function toggleMenu() {
-            const mobileMenu = document.getElementById('mobile-menu');
-            const menuIcon = document.getElementById('menu-icon');
-            const closeIcon = document.getElementById('close-icon');
-            
-            mobileMenu.classList.toggle('active');
-            
-            if (mobileMenu.classList.contains('active')) {
-                menuIcon.style.display = 'none';
-                closeIcon.style.display = 'block';
-            } else {
-                menuIcon.style.display = 'block';
-                closeIcon.style.display = 'none';
-            }
-        }
+  const mobileMenu = document.getElementById("mobile-menu");
+  const menuIcon = document.getElementById("menu-icon");
+  const closeIcon = document.getElementById("close-icon");
 
-        // Close mobile menu when clicking on a link
-        document.querySelectorAll('.mobile-menu a').forEach(link => {
-            link.addEventListener('click', () => {
-                toggleMenu();
-            });
-        });
+  mobileMenu.classList.toggle("active");
+
+  if (mobileMenu.classList.contains("active")) {
+    menuIcon.style.display = "none";
+    closeIcon.style.display = "block";
+  } else {
+    menuIcon.style.display = "block";
+    closeIcon.style.display = "none";
+  }
+}
+
+// Close mobile menu when clicking on a link
+document.querySelectorAll(".mobile-menu a").forEach((link) => {
+  link.addEventListener("click", () => {
+    toggleMenu();
+  });
+});
+
+function openFullscreen() {
+  let v = document.getElementById("v");
+  v.hidden = false;
+  v.play();
+  v.requestFullscreen();
+}
